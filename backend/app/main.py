c988262dc1820d74ef5f86cc9bdafb54280539b5
YashKrishnan24 <beastisfire01@gmail.com>
2026-09-07 23:50:48 +0530

    fix: CORS configuration to support credentials

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
